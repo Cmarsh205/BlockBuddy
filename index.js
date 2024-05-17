d7e41a0b2ebb19a6e3bb11feed629ec4b3f730a3
Cmarsh205 <156699625+Cmarsh205@users.noreply.github.com>
2024-05-16 21:38:30 -0230

    Update

--- index.js
+++ index.js
@@ -40,30 +40,26 @@ document.addEventListener("DOMContentLoaded", () => {
           }
         }
 
-        /**
-         * create container list div
-         * for each item in sets
-         *   create div
-         *   append header with set name
-         *   append div to container list
-         * append container list to document (featured sets element)
-         */
-
         const featuredSetsList = document.createElement("div");
 
         for (let i = 0; i < sets.length; i++) {
           const set = sets[i];
           const setDiv = document.createElement("div");
           const setHeader = document.createElement("h3");
+          const setPic = document.createElement("img")
           const setNumber = document.createElement("p");
 
           setHeader.innerText = set.name;
+          setPic.src = set.set_img_url;
           setNumber.innerText = set.set_num;
-
-          // setHeader.classList.add('')
-          // setHeader.id = ''
+        
+          setDiv.classList.add('setContainer')
+          setHeader.classList.add('setName')
+          setPic.classList.add('setImg')
+          setNumber.classList.add('setNum')
 
           setDiv.appendChild(setHeader);
+          setDiv.appendChild(setPic);
           setDiv.appendChild(setNumber);
           featuredSetsList.appendChild(setDiv);
         }
